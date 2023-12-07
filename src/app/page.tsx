4c5315a6ed819244bfb2d6b25f14176826d5317e
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-12-07 09:53:54 +0100

    Initial login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+
+import LoginForm from '@/app/LoginForm';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className='relative flex h-screen w-screen items-center justify-between	bg-gradient-to-bl from-indigo-500 to-indigo-900 p-4'>
+      <div>
+        <Image src='/login-illustration.png' width={632} height={632} alt='Login illustration' />
+      </div>
+      <LoginForm />
+    </main>
+  );
 }
