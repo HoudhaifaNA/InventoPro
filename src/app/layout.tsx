7321245239bed5008de1d27926dfa65688c71a1f
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-12-14 12:30:07 +0100

    Added overlfow hidden for the body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
+import clsx from 'clsx';
+
 import './globals.css';
 
 const poppins = Poppins({
@@ -15,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={poppins.className}>{children}</body>
+      <body className={clsx('overflow-y-hidden', poppins.className)}>{children}</body>
     </html>
   );
 }
