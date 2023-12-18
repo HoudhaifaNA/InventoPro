cb856ab4ef1b307ffe7d1c45392d3834802f9e83
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-12-18 18:12:28 +0100

    Added custom classnames for labeled input

--- src/components/LabeledInput/index.tsx
+++ src/components/LabeledInput/index.tsx
@@ -1,18 +1,18 @@
-import { ReactElement, cloneElement } from 'react';
+import { ComponentPropsWithoutRef, ReactElement, cloneElement } from 'react';
 import clsx from 'clsx';
 
-interface LabeledInputProps {
+interface LabeledInputProps extends Pick<ComponentPropsWithoutRef<'div'>, 'className'> {
   id?: string;
   label: string;
   color?: 'white' | 'black';
   children: ReactElement | JSX.Element;
 }
 
-const LabeledInput = ({ id, label, color = 'black', children }: LabeledInputProps) => {
+const LabeledInput = ({ id, label, color = 'black', className, children }: LabeledInputProps) => {
   const clonedChildren = cloneElement(children, { id });
 
   return (
-    <div>
+    <div className={className}>
       <label
         htmlFor={id}
         className={clsx('mb-2 inline-block text-sm font-medium', color === 'white' ? 'text-white' : 'text-black')}
