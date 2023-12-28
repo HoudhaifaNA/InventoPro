c303b86ed7bb8d8b9a12b8b96a0f77bcadb986d6
Houdhaifa <Houdhaifadeveloper@gmail.com>
2023-12-28 20:12:36 +0100

    First try at filtering and sorting products

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,8 +3,63 @@ import { type NextRequest, NextResponse } from 'next/server';
 import { db } from '@/db';
 import { ProductInsert, products } from '@/db/schema';
 
-export async function GET() {
+enum ORDER_BY {
+  NAME = 'name',
+  CREATED_AT = 'createdAt',
+  STOCK = 'stock',
+  RETAIL_PRICE = 'retailPrice',
+  WHOLESALE_PRICE = 'wholesalePrice',
+}
+
+function isOrderBy(param: string | null): param is ORDER_BY {
+  return Object.values(ORDER_BY).includes(param as any);
+}
+
+function getStockQuery(stock: string | null): number[] | undefined {
+  const stockArr: number[] = [];
+
+  if (stock && stock.split('_').length === 2) {
+    stock.split('_').forEach((value) => {
+      if (!isNaN(Number(value))) stockArr.push(Number(value));
+    });
+  }
+
+  return stockArr.length === 2 ? stockArr : undefined;
+}
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const orderBy = searchParams.get('orderBy');
+  const company = searchParams.get('company');
+  const category = searchParams.get('category');
+  const stock = searchParams.get('stock');
+  let orderByParam: string;
+  let order: 'asc' | 'desc';
+
+  if (orderBy && orderBy.length > 1) {
+    orderByParam = orderBy.startsWith('-') ? orderBy.split('-')[1] : orderBy;
+    order = orderBy.startsWith('-') ? 'desc' : 'asc';
+  }
+
+  const stockNumbers = getStockQuery(stock);
+
+  // STOCK STATUS HANDLING (FILTER AND SHOW)
+
+  // COMPANY AND CATEGORY HANDLING
+
+  // SALES TABLE
+
   const products = await db.query.products.findMany({
+    orderBy: (products, oper) => {
+      return isOrderBy(orderByParam) ? [oper[order](products[orderByParam])] : [oper.desc(products.createdAt)];
+    },
+    where: (products, { eq, between, and }) => {
+      let companyQuery = company ? eq(products.company, company) : undefined;
+      let categoryQuery = category ? eq(products.category, category) : undefined;
+      let stockQuery = stockNumbers ? between(products.stock, stockNumbers[0], stockNumbers[1]) : undefined;
+
+      return and(companyQuery, categoryQuery, stockQuery);
+    },
     with: {
       shipments: {
         columns: {
